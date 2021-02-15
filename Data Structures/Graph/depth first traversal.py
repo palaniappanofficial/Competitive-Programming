e41fd12d7922715673ee2c56fe9e8b7adfe69d95
palaniappanofficial <palanimohan61@gmail.com>
2021-02-15 23:58:47 +0530

    "Initial Commit"

---
+++ Data Structures/Graph/depth first traversal.py
@@ -0,0 +1,35 @@
+# Python3 program to print DFS traversal
+# from a given given graph
+from collections import defaultdict
+
+
+class Graph:
+
+	def __init__(self):
+		self.graph = defaultdict(list)
+
+	def addEdge(self, u, v):
+		self.graph[u].append(v)
+
+	def DFSUtil(self, v, visited):
+
+		visited.add(v)
+		print(v, end=' ')
+
+		for neighbour in self.graph[v]:
+			if neighbour not in visited:
+				self.DFSUtil(neighbour, visited)
+	def DFS(self, v):
+
+		visited = set()
+		self.DFSUtil(v, visited)
+
+obj = Graph()
+obj.addEdge(0, 1)
+obj.addEdge(0, 2)
+obj.addEdge(1, 2)
+obj.addEdge(2, 0)
+obj.addEdge(2, 3)
+obj.addEdge(3, 3)
+
+obj.DFS(2)
