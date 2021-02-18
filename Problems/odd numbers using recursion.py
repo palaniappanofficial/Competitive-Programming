3c5a9c99419516ee69b3f8f5c30ce3e31ebefe32
palaniappanofficial <palanimohan61@gmail.com>
2021-02-18 22:39:11 +0530

    "Initial Commit"

--- Problems/odd numbers using recursion.py
+++ Problems/odd numbers using recursion.py
@@ -1,16 +1,17 @@
-
-def oddnumbers(n,value,arr):
+arr=[]
+value=0
+def oddnumbers(n):
+    global value
+    global arr
     if value>n:
         return arr
     if value%2==1:
         arr.append(value)
     value=value+1
-    a=oddnumbers(n,value,arr)
+    a=oddnumbers(n)
     return a
 
 
 
-value=0
-arr=[]
-array=oddnumbers(100,value,arr)
+array=oddnumbers(100)
 print(array)
