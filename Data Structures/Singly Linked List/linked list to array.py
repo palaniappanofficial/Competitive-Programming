378b4b0091bfa714811bc5f14a9a06553656f1b2
palaniappanofficial <palanimohan61@gmail.com>
2021-02-20 21:31:26 +0530

    "Initial Commit"

---
+++ Data Structures/Singly Linked List/linked list to array.py
@@ -0,0 +1,32 @@
+class Linkedlist:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class Linklist:
+    def __init__(self):
+        self.head=None
+    def insertlast(self,data):
+        linklist=Linkedlist(data)
+        if self.head==None:
+            self.head=linklist
+        else:
+            temp=self.head
+            while(temp.next is not None):
+                temp=temp.next
+            temp.next=linklist
+    def printlist(self):
+        temp=self.head
+        while(temp is not None):
+            print(temp.data,end=" ")
+            temp=temp.next
+obj=Linklist()
+obj.insertlast(1)
+obj.insertlast(2)
+obj.insertlast(8)
+obj.insertlast(4)
+obj.insertlast(5)
+obj.printlist()
+
+
+
+
