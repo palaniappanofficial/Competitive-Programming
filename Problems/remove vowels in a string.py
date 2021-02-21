bbbbf66c92e038a42c8c5a3eb0e45a47e5c82e10
palaniappanofficial <palanimohan61@gmail.com>
2021-02-21 23:54:35 +0530

    Initial Commit

---
+++ Problems/remove vowels in a string.py
@@ -0,0 +1,11 @@
+class Problem:
+    def problems(self,string):
+        a=[]
+        a[:]=string
+        for i in a:
+            if i=="a" or i=="e" or i=="i" or i=="o" or i=="u" or i=="A" or i=="E" or i=="I" or i=="O" or i=="U":
+                a.remove(i)
+        print(a)
+        return a
+obj=Problem()
+obj.problems("howareyou")
