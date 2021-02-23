48057b11a52206fd539cd79917faafd3f3ba7bb6
palaniappanofficial <palanimohan61@gmail.com>
2021-02-23 21:40:32 +0530

    Initial Commit

---
+++ Problems/bitwise operators.py
@@ -0,0 +1,10 @@
+def bitwise(a,b):
+    print("Bitwise AND",a & b)
+    print("Bitwise OR",a | b)
+    print("Bitwise NOT",~ a )
+    print("Bitwise EX-OR",a^b)
+    print("Left Shift",a<<b)
+    print("Right Shift",a>>b)
+a=4
+b=5
+bitwise(a,b)
