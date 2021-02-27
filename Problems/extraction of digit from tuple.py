60f5bef01f461e4aa482bfeaf53a8d2d2032c741
palaniappanofficial <palanimohan61@gmail.com>
2021-02-27 22:58:36 +0530

    Initial Commit

---
+++ Problems/extraction of digit from tuple.py
@@ -0,0 +1,11 @@
+from itertools import chain
+a=[(1,2),(5,8),(9,7),(16,25),(27,22)]
+print(a)
+values=map(lambda b:b,chain.from_iterable(a))
+sets=set()
+print(values)
+for i in values:
+    for j in str(i):
+        print(i,end=" ")
+        sets.add(j)
+print(sets)
