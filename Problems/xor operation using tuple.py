132b4acdf392ae1feb99f4c3a0841b88bc38b837
palaniappanofficial <palanimohan61@gmail.com>
2021-02-28 21:09:05 +0530

    Initial Commit

---
+++ Problems/xor operation using tuple.py
@@ -0,0 +1,12 @@
+# Method 1
+a=(10,4,6,9)
+b=(5,2,3,3)
+
+c=[element1^element2 for element1,element2 in zip(a,b)]
+print(c)
+
+# Method 2
+
+from operator import xor
+d=tuple(map(xor,a,b))
+print(d)
