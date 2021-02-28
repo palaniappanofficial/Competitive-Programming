f7209dd8fba8dee576fe041d32eb1bd725970c3d
palaniappanofficial <palanimohan61@gmail.com>
2021-02-28 21:08:28 +0530

    Initial Commit

--- Problems/converting tuple matrix to tuple list.py
+++ Problems/converting tuple matrix to tuple list.py
@@ -1,9 +1,11 @@
+#Method 1
 test_list = [[(4, 5), (7, 8)],
              [(10, 13), (18, 17)],
              [(0, 4), (10, 1)]]
 test=[element for i in test_list for element in i]
 print(tuple(zip(*test)))
 
+#Method 2
 from itertools import chain
 a=chain.from_iterable(test_list)
 print(tuple(zip(*a)))
