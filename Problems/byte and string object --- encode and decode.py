1d16ead23e2111d3d9aee5ce30ccf37645feddb7
palaniappanofficial <palanimohan61@gmail.com>
2021-03-01 22:02:01 +0530

    Initial Commit

--- Problems/byte and string object --- encode and decode.py
+++ Problems/byte and string object --- encode and decode.py
@@ -1,8 +1,15 @@
 string_a="Hello Palaniappan"
 string_b=b"Hello Palaniappan"
 encodedresult=string_a.encode('ASCII')
-if result==string_b:
+if encodedresult==string_b:
     print("Encoded Successfully")
-    print("Encoded value is",result)
+    print("Encoded value is",encodedresult)
+else:
+    print("Not Encoded")
 
-decodedresult
+decodedresult=string_b.decode("ASCII")
+if string_a==decodedresult:
+    print("Decoded Successfully")
+    print("Decoded Value is",decodedresult)
+else:
+    print("Not Decoded")
