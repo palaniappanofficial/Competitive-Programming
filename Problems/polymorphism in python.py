b70a721bced299a8c6fc830d5f71d300591a7654
palaniappanofficial <palanimohan61@gmail.com>
2021-03-04 21:31:20 +0530

    Initial Commit

---
+++ Problems/polymorphism in python.py
@@ -0,0 +1,14 @@
+# Example 1
+print(len("Palaniappan"))
+print(len(['1','8','1','0','6','0']))
+
+# Example 2
+
+class new:
+    def add_function(self,a,b,c=0):
+        return a+b+c
+obj=new()
+a=obj.add_function(1,2,3)
+b=obj.add_function(25,3)
+print(a)
+print(b)
